9031acc6b4ed6d50a7027ba667e88effee2ed501
Zain Jerath <zainjerath@gmail.com>
2025-04-13 16:06:38 -0500

    fixed request button styling

--- src/FeedPage.tsx
+++ src/FeedPage.tsx
@@ -64,7 +64,16 @@ const FeedPage = () => {
           onClick={() => setShowRequests((prev) => !prev)}
           className="requests-button"
         >
-          💌
+          <span className="requests-content">
+            <svg
+              className="icon-white"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 6.5a4.5 4.5 0 1 1 4.5 4.5A4.49 4.49 0 0 1 12 6.5m6 6.5h-3a3 3 0 0 0-3 3v6h9v-6a3 3 0 0 0-3-3M6.5 6A3.5 3.5 0 1 0 10 9.5 3.5 3.5 0 0 0 6.5 6m1 9h-2A2.5 2.5 0 0 0 3 17.5V22h7v-4.5A2.5 2.5 0 0 0 7.5 15" />
+            </svg>
+            Tastemate Requests
+          </span>
         </button>
       </div>
       <PostModal isOpen={isPostModalOpen} onClose={() => setPostModalOpen(false)} />
